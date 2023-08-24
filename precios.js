e0c419b7caa0619b95dc459738d2f44cdd3b6ff2
LUIS_FEDORA <garcia.luistomasnicolas@gmail.com>
2023-08-24 20:00:55 -0300

    modified:   precios.html 	modified:   precios.js

--- precios.js
+++ precios.js
@@ -1,5 +1,7 @@
 var precios = {
-    particular: 30,
-    sobreturno: 40,
+    particular: 8000,
+    presencial: 10000,
+    sobreturno: 12000,
+    online: 8000,
     // ... otros valores de precios ...
 };
